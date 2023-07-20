380e927bfbe40fcc08985793dc21d11fbae84621
hailin0 <wanghailin@apache.org>
2023-07-20 11:08:02 +0800

    [Improve][Oracle][CDC] Optimize logminer latency & archivelog bloat

--- seatunnel-connectors-v2/connector-cdc/connector-cdc-oracle/src/main/java/org/apache/seatunnel/connectors/seatunnel/cdc/oracle/config/OracleSourceConfigFactory.java
+++ seatunnel-connectors-v2/connector-cdc/connector-cdc-oracle/src/main/java/org/apache/seatunnel/connectors/seatunnel/cdc/oracle/config/OracleSourceConfigFactory.java
@@ -78,6 +78,10 @@ public class OracleSourceConfigFactory extends JdbcSourceConfigFactory {
         props.setProperty("log.mining.batch.size.max", String.valueOf(2147483646));
         props.setProperty("log.mining.batch.size.min", String.valueOf(2000));
 
+        // Optimize logminer latency
+        props.setProperty("log.mining.strategy", "online_catalog");
+        props.setProperty("log.mining.continuous.mine", String.valueOf(true));
+
         if (tableList != null) {
             // Oracle identifier is of the form schemaName.tableName
             props.setProperty(
